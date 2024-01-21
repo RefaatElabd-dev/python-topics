01337d422931400e378b041ec0358cb3110b2651
Refaat Elabd <RefaatElabd7@gmail.com>
2024-01-22 00:55:28 +0200

    DoublyLinkedList full functionality

---
+++ DataStructure/DoublyLinkedList/imlementation.py
@@ -0,0 +1,124 @@
+class Node:
+    def __init__(self, value):
+        self.next = None
+        self.prev = None
+        self.value = value
+        
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
+    
+    def pop(self):
+        if self.head is None:
+            return None
+        if self.length == 1:
+            temp = self.head
+            self.head = None
+            self.tail = None
+        else:
+            temp = self.tail
+            self.tail = temp.prev
+            self.tail.next = None
+            temp.prev = None
+        self.length -= 1
+        return temp
+    
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.head.prev = new_node
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
+    
+    def pop_first(self):
+        if self.head is None:
+            return None
+        if self.length == 1:
+            temp = self.head
+            self.head = None
+            self.tail = None
+        else:
+            temp = self.head
+            self.head = temp.next
+            self.head.prev = None
+            temp.next = None
+        self.length -= 1
+        return temp
+    
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index < self.length / 2:
+            temp = self.head
+            for _ in range(0, index, 1):
+                temp = temp.next
+        else:
+            temp = self.tail
+            for _ in range(self.length - 1, index, -1):
+                temp = temp.prev
+        return temp
+    
+    def set_value(self, index, value):
+        target_node = self.get(index)
+        if target_node is not None:
+            target_node.value = value
+            return True
+        return False
+    
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if(index == self.length):
+            return self.append(value)
+        new_node = Node(value)
+        temp = self.get(index)
+        new_node.prev = temp.prev
+        new_node.next = temp
+        temp.prev.next = new_node
+        temp.prev = new_node
+        self.length += 1
+        return True
+        
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+        temp = self.get(index)
+        temp.prev.next = temp.next
+        temp.next.prev = temp.prev
+        temp.next = None
+        temp.prev = None
+        self.length -= 1
+        return temp
+        
