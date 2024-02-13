0c47073ffd2057936483c5cd1e81dcb484ee1126
Refaat Elabd <RefaatElabd7@gmail.com>
2024-02-13 04:33:26 +0200

    find_max_min problem

---
+++ diffrent_problems/find_max_min.py
@@ -0,0 +1,21 @@
+def find_max_min(myList):
+    max_value = myList[0]
+    min_value = myList[0]
+    for item in myList:
+        if item < min_value:
+            min_value = item
+        if item > max_value:
+            max_value = item
+    return (max_value, min_value)
+    
+
+
+print( find_max_min([5, 3, 8, 1, 6, 9]) )
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    (9, 1)
+    
+"""
